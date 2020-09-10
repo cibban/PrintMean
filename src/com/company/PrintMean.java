cb644174094ca80cc26ed294bab86531e986b5d6
Patrik Sjöstrand <patrik.sjostrand@student.kyh.se>
2020-09-10 14:37:56 +0200

    Fixed bugs in description and means calc.

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -5,12 +5,12 @@ public class PrintMean {
     public static void main(String[] args) {
         //Heltalsdivision (lkajsdhbfkjasdf)
 
-        System.out.println("Mean of 0, 1, 2, 3: " + meanCalc(1, 1, 3, 3));
+        System.out.println("Mean of 0, 1, 2, 3: " + meanCalc(0, 1, 2, 3));
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1, 1, 3, 3));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1, 2, 3, 4));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1, -2, 3, -4));
         System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1, 11, 24, 2));
-        System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-11, 124, 23, 2));
+        System.out.println("Mean of -11, 124, 23, 2: " + meanCalc(-11, 124, 23, 2));
     }
 
     public static String meanCalc(int a, int b, int c, int d) {
