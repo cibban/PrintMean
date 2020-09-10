8a462d65ee602d8b66ff6fd49cc8f5ca0c055713
Patrik Sjöstrand <patrik.sjostrand@student.kyh.se>
2020-09-10 14:33:38 +0200

    Added 0,1,2,3

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -5,6 +5,7 @@ public class PrintMean {
     public static void main(String[] args) {
         //Heltalsdivision (lkajsdhbfkjasdf)
 
+        System.out.println("Mean of 0, 1, 2, 3: " + meanCalc(1, 1, 3, 3));
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1, 1, 3, 3));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1, 2, 3, 4));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1, -2, 3, -4));
