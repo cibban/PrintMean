36494ac379991d083c5dcb19abba244b1c1e74df
Patrik Sjöstrand <patrik.sjostrand@student.kyh.se>
2020-09-10 13:37:09 +0200

    Revert "Added comment"
    This reverts commit 1216bf569fab428139a2f5dba38c04a6735ab73c.

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -3,7 +3,7 @@ package com.company;
 public class PrintMean {
 
     public static void main(String[] args) {
-        //Heltalsdivision (klart)
+        //Heltalsdivision (skriver ut int istället för double)
 
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1, 1, 3, 3));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1, 2, 3, 4));
